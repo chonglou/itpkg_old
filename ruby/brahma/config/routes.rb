a5917b5ffef562168145eae889f125d03e43bdfc
flamen <zhengjitang@gmail.com>
2014-02-14 12:19:46 -0800

    ruby版本

--- ruby/brahma/config/routes.rb
+++ ruby/brahma/config/routes.rb
@@ -5,7 +5,7 @@ Brahma::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'site#index'
+  root 'site#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +14,7 @@ Brahma::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  #resources :posts
 
   # Example resource route with options:
   #   resources :products do
