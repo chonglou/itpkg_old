0027b879b3df9708baaecc08942e0e76ce1dfbe9
flamen <jitang.zheng@gmail.com>
2014-10-22 16:18:28 -0700

    文件上传设置

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
+
+  mount_uploader :avatar, AvatarUploader
 end
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'carrierwave'
+require 'carrierwave/orm/activerecord'
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+
+CarrierWave.configure do |config|
+  config.permissions = 0600
+  config.directory_permissions = 0700
+  config.storage = :fog
+
+  config.fog_credentials = Hash[JSON.parse(ENV['ITPKG_ATTACHMENT_CREDENTIALS']).map{ |k, v| [k.to_sym, v] }]
+  config.fog_directory = ENV['ITPKG_ATTACHMENT_BUCKET']
+  config.fog_public = true
+  config.fog_attributes = {'Cache-Control' => "max-age=#{365.day.to_i}"}
+end
