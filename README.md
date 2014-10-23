71d82a5e4053a8c32f5a77d758bc2a6db1ca1356
flamen <jitang.zheng@gmail.com>
2014-10-23 15:43:06 -0700

    git 工具类

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ IT-PACKAGE
     rake itpkg:env
 
 #### Background Worker
-    sidekiq -d # start new worker
+    bundle exec sidekiq -d # start new worker
     ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -USR1 # stop all workers
--- db/migrate/20141023212923_create_repository_logs.rb
+++ db/migrate/20141023212923_create_repository_logs.rb
@@ -2,9 +2,11 @@ class CreateRepositoryLogs < ActiveRecord::Migration
   def change
     create_table :repository_logs do |t|
       t.integer :repository_id, null:false
-      t.integer :user_id, null:false
-      t.text :message, null:false
-      t.timestamps
+      t.string :branch, null:false
+      t.string :name, null:false
+      t.string :email, null:false
+      t.string :message, null:false
+      t.datetime :created, null:false
     end
   end
 end
