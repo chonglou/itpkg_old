23fe399f9c5a61ef95b92eb652a12e61930f3729
flamen <jitang.zheng@gmail.com>
2014-10-23 22:07:44 -0700

    联系人信息数据库设计

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024045714) do
+ActiveRecord::Schema.define(version: 20141024050540) do
+
+  create_table "contacts", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.string   "logo",       null: false
+    t.string   "username",   null: false
+    t.text     "content",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contacts", ["username"], name: "index_contacts_on_username", using: :btree
 
   create_table "documents", force: true do |t|
     t.integer  "project_id",                        null: false
