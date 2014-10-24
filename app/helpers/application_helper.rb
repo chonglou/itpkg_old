fa2a977938d1dcebdd03cbc7bcef98531e6f47c2
flamen <jitang.zheng@gmail.com>
2014-10-24 16:02:47 -0700

    引入图标

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def nav_links
+    links = {root_path => t('links.home')}
+    if current_user
+      links['111'] = t('links.personal')
+    end
+    links[about_me_path]=t('web.title.about_me')
+    links
+
+  end
+
+   def personal_bar
+    if current_user
+      label = "#{t('labels.welcome')}, #{current_user.email}"
+      links={
+          '111' => t('links.personal'),
+          '222' => t('links.logout')
+      }
+    else
+      label = t('labels.register_or_signin')
+      links={
+          '111' => t('links.login')
+      }
+    end
+    {label: label, links: links}
+  end
+
 end
