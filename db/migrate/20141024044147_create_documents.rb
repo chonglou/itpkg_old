5f840a96b73ac21287d7e261472299fdce950236
flamen <jitang.zheng@gmail.com>
2014-10-23 21:45:00 -0700

    文档数据库

---
+++ db/migrate/20141024044147_create_documents.rb
@@ -0,0 +1,13 @@
+class CreateDocuments < ActiveRecord::Migration
+  def change
+    create_table :documents do |t|
+      t.integer :project_id, null:false
+      t.integer :creator_id, null:false
+      t.string :title, null:false
+      t.string :name, null:false, limit:36
+      t.string :ext, null:false, limit:5
+      t.integer :status, null:false, default:0, limit:2
+      t.timestamps
+    end
+  end
+end
