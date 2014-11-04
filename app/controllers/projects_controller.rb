26898dea96b7fd01951a5bcc4810607c16db0288
flamen <jitang.zheng@gmail.com>
2014-11-04 10:27:27 -0800

    fix projects list bug

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,10 @@
+require 'itpkg/utils/string_helper'
+
 class ProjectsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @projects = Project.select(:id, :name, :details).where(creator_id:current_user.id).map{|p| {url:project_path(p.id), name:p.name, details:p.details}}
+    @projects = Project.select(:id, :name, :details).where(creator_id:current_user.id).map{|p| {url:project_path(p.id), name:p.name, details: Itpkg::StringHelper.md2html(p.details)}}
   end
 
   def new
@@ -22,7 +24,39 @@ class ProjectsController < ApplicationController
   end
 
   def show
-
+    @project = Project.find_by params[:id]
+    @items = [
+        {
+            url: document_show_path(name: 'help'),
+            logo: 'flat/call37.png',
+            label: t('links.about_us')
+        },
+        {
+            url: document_show_path(name: 'help'),
+            logo: 'flat/call37.png',
+            label: t('links.about_us')
+        },
+        {
+            url: document_show_path(name: 'help'),
+            logo: 'flat/call37.png',
+            label: t('links.about_us')
+        },
+        {
+            url: document_show_path(name: 'help'),
+            logo: 'flat/coins24.png',
+            label: t('links.finance')
+        },
+        {
+            url: document_show_path(name: 'help'),
+            logo: 'flat/multiple25.png',
+            label: t('links.contact')
+        },
+        {
+            url: document_show_path(name: 'help'),
+            logo: 'flat/call37.png',
+            label: t('links.about_us')
+        }
+    ]
   end
 
   def edit
