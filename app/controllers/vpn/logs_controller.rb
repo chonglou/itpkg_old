32e6e52fc4debd3db96f8db301f531110725cb52
flamen <jitang.zheng@gmail.com>
2014-11-04 23:38:10 -0800

    vpn logs index vpn users index

--- app/controllers/vpn/logs_controller.rb
+++ app/controllers/vpn/logs_controller.rb
@@ -1,5 +1,6 @@
 class Vpn::LogsController < ApplicationController
   before_action :must_admin!
   def index
+    @items = Vpn::Log.select(:username, :message, :created).last(200).map{|l| "#{l.created}[#{l.username}]: #{l.message}"}
   end
 end
--- app/controllers/vpn/users_controller.rb
+++ app/controllers/vpn/users_controller.rb
@@ -1,22 +1,34 @@
 class Vpn::UsersController < ApplicationController
   before_action :must_admin!
-  def index
-  end
-  def new
 
+  def index
+    @buttons = [
+        {label: t('links.vpn_user.create'), url: new_vpn_user_path, style: 'primary'},
+        {label: t('links.vpn_log.list'), url: vpn_logs_path, style: 'warning'}
+    ]
+    @users = Vpn::User.select(:id, :name, :email, :enable, :start_date, :end_date).map { |u| [u.id, u.name, u.email, u.enable, u.start_date, u.end_date] }
   end
+
+  def new
+    @user = Vpn::User.new
+  end
+
   def create
 
   end
+
   def show
 
   end
+
   def edit
 
   end
+
   def update
 
   end
+
   def destroy
 
   end
