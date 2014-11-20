7f3cf1c96602e000977ef4810d5ec9be6c4dba5c
flamen <jitang.zheng@gmail.com>
2014-11-20 01:15:49 -0800

    add tig user

--- lib/itpkg/utils/gitolite.rb
+++ lib/itpkg/utils/gitolite.rb
@@ -7,12 +7,18 @@ module Itpkg
     def admin!
       git_admin = "#{Rails.root}/tmp/storage/gitolite-admin"
       unless Dir.exist?(git_admin)
-        begin
-          Rugged::Repository.clone_at 'localhost:gitolite-admin', git_admin
-        rescue =>e
-          Rails.logger.error e
-        end
+        Rugged::Repository.clone_at "#{Setting.git_admin_host}:gitolite-admin", git_admin, {
+            credentials: ssh_key_credential
+        }
       end
+    end
+
+    def ssh_key_credential
+      Rugged::Credentials::SshKey.new({
+                                          username: Setting.git_admin_username,
+                                          publickey: Setting.git_admin_pub_key,
+                                          privatekey: Setting.git_admin_key
+                                      })
     end
   end
 end
--- test/integration/gitolite_test.rb
+++ test/integration/gitolite_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
+require 'itpkg/utils/gitolite'
+require 'fileutils'
+
 
 class GitoliteTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  ADMIN_PATH = "#{Rails.root}/tmp/storage/gitolite-admin"
+
+  test 'clone admin' do
+    assert @ig.admin!
+  end
+
+  def setup
+    @ig = Itpkg::Gitolite
+    if Dir.exist?(ADMIN_PATH)
+      FileUtils.mv ADMIN_PATH, "#{ADMIN_PATH}-bak"
+    end
+    Setting.git_admin_host = 'localhost'
+    Setting.git_admin_username = ENV['USER']
+    Setting.git_admin_pub_key = "#{ENV['HOME']}/.ssh/id_rsa.pub"
+    Setting.git_admin_key = "#{ENV['HOME']}/.ssh/id_rsa"
+
+  end
+
+  def teardown
+    if Dir.exist?("#{ADMIN_PATH}-bak")
+      FileUtils.mv "#{ADMIN_PATH}-bak", ADMIN_PATH
+    end
+    FileUtils.rm_r ADMIN_PATH
+  end
+
 end
