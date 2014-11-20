76fcd8b9ac48982bbbc7a28d772dfe0eaf9b94b4
flamen <jitang.zheng@gmail.com>
2014-11-20 06:37:30 -0800

    git 工作者进程

--- app/workers/git_worker.rb
+++ app/workers/git_worker.rb
@@ -4,11 +4,13 @@ class GitWorker
   include Sidekiq::Worker
 
   def export
-    ig = Itpkg::Gitolite
-    ig.open
-    ig.export
-    ig.push
-    ig.close
+    if Setting.git_modify_flag
+      ig = Itpkg::Gitolite
+      ig.open
+      ig.export
+      ig.push
+      ig.close
+    end
   end
 
 end
