1f70048cf30a661a5e27fa6775c92006b761f1b2
flamen <jitang.zheng@gmail.com>
2014-11-22 01:19:58 -0800

    添加附件

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 class UserMailer < ActionMailer::Base
   def key_pairs(user_id)
     u = User.find user_id
-    #todo
+    attachments['id_rsa'] = u.ssh_key.private_key
+    attachments['id_rsa.pub'] = u.ssh_key.public_key
     mail( to:u.email, subject: t('mails.key_pairs.subject')) if u
   end
 end
