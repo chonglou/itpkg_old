671b33de377fa91cf0976209eaf89669047b4c1a
flamen <jitang.zheng@gmail.com>
2014-12-06 11:56:16 -0800

    import kaminari

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -25,7 +25,7 @@ class StatusController < ApplicationController
 
   def logs
     @index = 2
-    @items = BgLog.order(_id: :desc).page(1).per(50).map {|l| "#{l.created}: #{l.message}"}
+    @items = BgLog.order(_id: :desc).page
     render 'logs', layout: 'status/view'
   end
 end
