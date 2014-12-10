5f2597db174342b104a05c53c919aa82c185d83e
flamen <jitang.zheng@gmail.com>
2014-12-10 14:02:49 -0800

    wiki 管理

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,5 +1,6 @@
 class WikisController < ApplicationController
   before_action :authenticate_user!
+  before_action :_check_author, only:[:edit, :update, :destroy]
 
   def index
     @buttons = [
@@ -9,12 +10,49 @@ class WikisController < ApplicationController
     @wikis = Wiki.order(updated_at: :desc).page(params[:page])
   end
 
+  def show
+    @wiki = Wiki.find params[:id]
+  end
+
   def new
     @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new _wiki_params
+    if @wiki.save
+      current_user.add_role :author, @wiki
+      redirect_to wiki_path(@wiki)
+    else
+      render 'new'
+    end
+  end
+
+
+  def update
+    @wiki = Wiki.find params[:id]
+    if @wiki.update(_wiki_params)
+      redirect_to wiki_path(@wiki)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @wiki.destroy
+    redirect_to wikis_path
   end
 
   private
   def _wiki_params
     params.require(:wiki).permit(:title, :body)
+  end
+
+  def _check_author
+
+    @wiki = Wiki.find(params[:id])
+    unless current_user.is_author_of?(@wiki)
+      render status: 404
+    end
   end
 end
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,8 @@
 class Wiki < ActiveRecord::Base
-  belongs_to :project
+  resourcify
+  paginates_per 5
 
   validates :title, :body, presence: true
 
-  enum status: {project: 0, personal: 1, publish: 2}
+  enum status: {personal: 1, publish: 0}
 end
