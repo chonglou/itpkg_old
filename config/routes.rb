9e0061c9e7f6d6f047330739d52b29dde9ec219e
Sen-Zhang <senzhang1987@126.com>
2015-01-26 21:26:39 -0800

    enable task to update status by clicking buttons, and set real start and finish time

--- config/routes.rb
+++ config/routes.rb
@@ -115,6 +115,8 @@ Rails.application.routes.draw do
         resources :story_comments, controller: 'projects/story_comments'
         resources :tasks, controller: 'projects/tasks' do
           resources :task_comments, controller: 'projects/task_comments'
+
+          post :update_status
         end
 
         post :update_status
