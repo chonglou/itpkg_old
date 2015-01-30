468dc400888c358f6049ccdaed300d95b656ad25
flamen <jitang.zheng@gmail.com>
2015-01-29 21:01:47 -0800

    fix message add bug

--- lib/itpkg/utils/xmpp.rb
+++ lib/itpkg/utils/xmpp.rb
@@ -18,16 +18,18 @@ module Itpkg
         @client.add_message_callback do |m|
           #yield m.from, @email, m.body, m.type
           begin
-            ChatJob.perform_later m.from.to_s, @email, m.body, m.type.to_s, Time.now.to_s
+            #ChatJob.perform_later m.from.to_s, @email, m.body, m.type.to_s, Time.now.to_s
+            ChatMessage.create from: m.from, to: @email, body: m.body, flag: ChatMessage.flags[m.type], created: Time.now
           rescue => e
             puts e
           end
         end
       end
 
-      def send!(to, body)
+      def send!(to, body, type=:chat)
+        ChatMessage.create from: @email, to: to, body: body, flag: ChatMessage.flags[type], created: Time.now
         msg = Jabber::Message.new(to, body)
-        msg.type=:chat
+        msg.type=type
         @client.send msg
       end
 
